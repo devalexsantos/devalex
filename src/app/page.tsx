7794020ba710e8c363a8d5a430362f9890a1327e
Alex Santos <devalexsantos@gmail.com>
2023-08-06 17:30:57 -0300

    aditioned featured projects session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
 import AvatarBox from "@/components/AvatarBox"
-import SubTitle from "@/components/SubTitle"
-import Title from "@/components/Title"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+import Bio from "@/layout/Bio"
+import FeaturedProjects from "@/layout/FeaturedProjects"
 
 
 export default function Home() {
   return (
     <main className='m-auto max-w-5xl flex flex-col p-4'>
       <AvatarBox />
-      <div className="max-w-[470px]">
-        <Title text="Desenvolvedor Front, comunicador e amante de café ☕" />
-        <SubTitle>
-          <p>Meu nome é Alex Santos, tenho 30 anos e trabalho há mais de 10 como 
-          {' '}<Popover>
-            <PopoverTrigger className="underline">Desenvolvedor Front</PopoverTrigger>
-            <PopoverContent className="text-zinc-700">Mais de 05 anos de experiência no Desenvolvimento de <strong>Sites Institucionais</strong>, <strong>Landing Pages</strong>, <strong>E-Commerces</strong>, <strong>Dashboards Corporativos</strong>, etc.</PopoverContent>
-          </Popover>
-            , 
-            {' '}<Popover>
-            <PopoverTrigger className="underline">Designer</PopoverTrigger>
-            <PopoverContent className="text-zinc-700"><strong>Certificado Adobe</strong> em todas as ferramentas de Design Gráfico da Adobe, domínio completo do <strong>Figma</strong> e mais de <strong>10 anos</strong> atuando com tarefas de Design.</PopoverContent>
-          </Popover>
-            , Vendedor e resolvedor de problemas.</p>
-          <p>Tenho domínio em Javascript/Typescript e tenho facilidade em trabalhar com React/Next.js Figma, Adobe Collection.</p>
-        </SubTitle>
-      </div>
+      <Bio />
+      <FeaturedProjects />
     </main>
   )
 }
